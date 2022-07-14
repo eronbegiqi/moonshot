8bdc339e3ca24bd2bff4ed9a6b52a842869854b6
majorfenix13 <eronbegiqi42@gmail.com>
2022-07-15 00:04:52 +0200

    new files added fontawesome coin-page

--- script.js
+++ script.js
@@ -1,42 +1,78 @@
-const btn = document.getElementById('menu-btn')
-const nav = document.getElementById('menu')
+const btn = document.getElementById("menu-btn");
+const nav = document.getElementById("menu");
 
-btn.addEventListener('click', () => {
-  btn.classList.toggle('open')
-  nav.classList.toggle('flex')
-  nav.classList.toggle('hidden')
-})
+btn.addEventListener("click", () => {
+  btn.classList.toggle("open");
+  nav.classList.toggle("flex");
+  nav.classList.toggle("hidden");
+});
 
-/* Make negativ number go red */
-function MakeNegative() {
-  TDs = document.getElementsByTagName('td');
-  for (var i=0; i<TDs.length; i++) {
-          var temp = TDs[i];
-          if (temp.firstChild.nodeValue.indexOf('-') == 0) temp.className = "negative";
-      }
+//Make color change based on number positive or negative
+function MakePosNeg(n) {
+  for (var i = 0; i < n.length; i++) {
+    var temp = n[i];
+    if (temp.firstChild.nodeValue.indexOf("-") < 0) {
+      temp.className = "positive";
+    } else {
+      temp.className = "negative";
+    }
+  }
 }
+var TDs = document.querySelectorAll(".plusmin");
+const column = document.querySelectorAll(`td:nth-child(6)`);
+const column2 = document.querySelectorAll(`td:nth-child(8)`);
 
-MakeNegative();
-/* Make positive number go green */
-function MakePositive() {
-  TDs = document.getElementsByTagName('td');
-  for (var i=0; i<TDs.length; i++) {
-          var temp = TDs[i];
-          if (temp.firstChild.nodeValue.indexOf[4] >= 0) temp.className = "positive";
-      }
-}
-MakePositive();
+onload = MakePosNeg(column);
+onload = MakePosNeg(column2);
+onload = MakePosNeg(TDs);
+///
 
-function filled(){
-  var Image_Id = document.getElementById('filled-img');
-            if (Image_Id.src.match("assets/Star 1.svg")) {
-                Image_Id.src = "assets/Star 2.svg";
-            }
-            else {
-                Image_Id.src = "assets/Star 1.svg";
-            }
+function filled() {
+  var Image_Id = document.getElementById("filled-img");
+  if (Image_Id.src.match("assets/Star 1.svg")) {
+    Image_Id.src = "assets/Star 2.svg";
+  } else {
+    Image_Id.src = "assets/Star 1.svg";
+  }
 }
 
 //Tool Tip
-const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
-const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+tippy(".tip-wl", {
+  content: "Add to watchlist!",
+  arrow: true,
+  followCursor: true,
+});
+
+// tippy(".filter-2", {
+//   content: "Add to watchlist!",
+//   arrow: true,
+//   placement: 'bottom',
+//   animation: 'shift-toward-extreme',
+//   trigger: 'click',
+//   followCursor: true,
+//   allowHTML: true,
+// });
+
+tippy(".lang", {
+  content: "",
+});
+
+// Copy text
+
+
+const copyToClipboard = (elementId) => {
+  const text = document.getElementById(elementId).innerHTML;
+  navigator.clipboard
+    .writeText(text)
+    .then(() => {
+      tippy(".btn-0", {
+        content: "Copied",
+        arrow: true,
+        followCursor: true,
+        showOnCreate: true,
+      });
+    })
+    .catch((err) => {
+      console.error(`Error copying text to clipboard: ${err}`);
+    });
+};
